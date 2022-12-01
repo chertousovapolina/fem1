607f436bf90af1d17d8bd463cbe64b30a7d88cf4
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-12-01 20:59:36 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+
+struct gnom{
+char sex;
+int age;
+int height;
+};
+int main() {
+	setlocale(LC_ALL, "ru");
+	gnom a, b, c;
+	cin >> a.age >> a.height >> a.sex;
+	cin >> b.age >> b.height >> b.sex;
+	cin >> c.age >> c.height >> c.sex;
+	int female=0;
+	if (a.sex == 'f') female++;
+	if (b.sex == 'f') female++;
+	if (c.sex == 'f') female++;
+	if (female >= 2) cout << "матриархат";
+	else cout << "патриархат";
+
+	
+
+}
